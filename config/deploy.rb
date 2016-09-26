97666cea301ef5dcc8f1961119455ec2b521d633
Julio Plasencia <julioplasencia@gmail.com>
2016-09-25 22:27:47 -0400

    TEST

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_url, 'git@github.com:jplasencia/devise.git'
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/deploy/devise'
 set :rbenv_path, '/home/deploy/.rbenv/bin/rbenv'
-'
+
 
 # Default value for :scm is :git
 # set :scm, :git
